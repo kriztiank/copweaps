f4530db419cbfb5df37f505dbe4ce7b461b4ea5f
kriztiank <kriztiank@gmail.com>
2020-02-24 13:04:17 +0100

    generel updates

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // import './scss/custom';
+
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
+
+// Init Scrollspy
+$('body').scrollspy({ target: '#main-nav' });
+
+// Smooth Scrolling
+$('#main-nav a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const { hash } = this;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+      function() {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
